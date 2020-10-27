ee5a7f26c0dcab73b07c77e0b5a54ed910ed16d6
drapaiton <drapaiton@gmail.com>
2020-10-27 16:15:18 -0700

    yo

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	headparser: require('./headParser').default,
-	// filemeta: require('./filemeta').default,
-	// timestamp: require('./timestamp').default,
-	// tracker: require('./tracker').default,
-	// urlshort: require('./urlshort').default,
-};
---
+++ src/timeStamp.ts
@@ -0,0 +1,22 @@
+export default () => {};
+// const express = require('express');
+// const path = require('path');
+// const serverless = require('serverless-http');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+// const router = express.Router();
+// router.get('/', (_, res) => {
+//   res.send('ok')
+// });
+
+// router.get('/okey', (_, res) => {
+//   res.send('okboomer')
+// });
+
+// app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (_, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
