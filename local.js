19b7554053470b5cbd1baacb840d90543565aaa2
drapaiton <drapaiton@gmail.com>
2020-10-28 09:32:42 -0600

    added dependencies

---
+++ local.js
@@ -0,0 +1,17 @@
+'use strict';
+console.clear()
+
+let choices =[ 
+"filemeta", 
+"headparser",
+"server",
+"shorturl",
+"timestamp",
+"tracker" ]
+let prompt = require('readline-sync').question(`${choices}\n\nchoose one :) ->\n`)
+
+let choice = choices.filter(v=>v.match(prompt))[0]
+console.log(choice)
+const app = require('./build/' + choice);
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
