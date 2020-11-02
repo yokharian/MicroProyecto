0b60e38c5634057382658e7c3da46a3792eda534
drapaiton <drapaiton@gmail.com>
2020-11-02 13:31:51 -0600

    debug mode

--- local.js
+++ local.js
@@ -1,17 +1,20 @@
 'use strict';
-console.clear()
+console.clear();
 
-let choices =[ 
-"filemeta", 
-"headparser",
-"server",
-"shorturl",
-"timestamp",
-"tracker" ]
-let prompt = require('readline-sync').question(`${choices}\n\nchoose one :) ->\n`)
-let choice = choices.filter(v=>v.match(prompt))[0]
-console.log(choice)
+let choices = [
+	'filemeta',
+	'headparser',
+	'server',
+	'shorturl',
+	'timestamp',
+	'tracker',
+];
+let prompt = require('readline-sync').question(
+	`${choices}\n\nchoose one :) ->\n`,
+);
+let choice = choices.filter(v => v.match(prompt))[0];
+// let choice = 'filemeta';
+console.log(choice);
 const app = require('./build/' + choice);
-
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
